cd2480764445e4f2db7b4106e4d4018d77f5e6ee
Cdt-Raghib <pyraghib@gmail.com>
2025-10-05 21:45:00 -0700

    Update notificationbar.py

--- utils/notificationbar.py
+++ utils/notificationbar.py
@@ -14,9 +14,10 @@ class NotificationBar(MDSnackbar):
     color = StringProperty("green")
     button_text = StringProperty()
 
-    def play_sound(self):
+    def play_sound(self, error):
         s = SoundLoader.load('assets/audio/error-sound.mp3')
-        s.play()
+        if s and error:
+            s.play()
         
     def action(self):
         file_path = os.path.abspath("assets/data/help.html")
@@ -24,7 +25,7 @@ class NotificationBar(MDSnackbar):
 
     def open_with_text(self, text, error=False):
         self.text = text
-        
+        self.play_sound(error)
         if error:
             self.button_text = 'Help'
             self.color = 'red'
